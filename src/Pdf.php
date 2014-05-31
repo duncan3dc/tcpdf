58f4847410d980957d77c5cd38b351cfe205dd6b
Craig Duncan <git@duncanc.co.uk>
2014-05-31 19:30:10 +0100

    Created a simple wrapper around tcpdf

---
+++ src/Pdf.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace duncan3dc;
+
+use \duncan3dc\Helpers\Helper;
+
+class Pdf extends \TCPDF {
+
+    public  $headerFunction;
+    public  $footerFunction;
+
+    public  $defaultCellHeight;
+
+
+    public function __construct($options=false) {
+
+        $options = Helper::getOptions($options,[
+            "orientation"   =>  "P",
+            "unit"          =>  "mm",
+            "format"        =>  "A4",
+            "unicode"       =>  true,
+            "encoding"      =>  "UTF-8",
+            "diskcache"     =>  false,
+            "addpage"       =>  true,
+            "pagebreaks"    =>  true,
+            "header"        =>  false,
+            "footer"        =>  false,
+        ]);
+
+        parent::__construct($options["orientation"],$options["unit"],$options["format"],$options["unicode"],$options["encoding"],$options["diskcache"]);
+
+        $this->setPrintHeader($options["header"]);
+        $this->setPrintFooter($options["footer"]);
+
+        # Set auto page breaks
+        $this->SetAutoPageBreak($options["pagebreaks"]);
+
+        # Set font
+        $this->SetFont("dejavusans","",10);
+
+        # Set the default margin
+        $this->SetMargins(4,4);
+
+        # Add a page
+        if($options["addpage"]) {
+            $this->AddPage();
+        }
+
+        $this->headerFunction = false;
+        $this->footerFunction = false;
+
+        $this->defaultCellHeight = 0;
+
+    }
+
+
+    public function addCell($options) {
+
+        $o = Helper::getOptions($options,[
+            "width"     =>  0,
+            "height"    =>  $this->defaultCellHeight,
+            "content"   =>  "",
+            "border"    =>  0,
+            "align"     =>  "",
+            "fill"      =>  false,
+            "link"      =>  "",
+            "stretch"   =>  0,
+            "calign"    =>  "T",
+            "valign"    =>  "M",
+            "style"     =>  "",
+        ]);
+
+        if($style = $o["style"]) {
+            $current = $this->getFontStyle();
+            $this->SetFont("",$style);
+        }
+
+        $return = $this->Cell($o["width"],$o["height"],$o["content"],$o["border"],0,$o["align"],$o["fill"],$o["link"],$o["stretch"],false,$o["calign"],$o["valign"]);
+
+        if($style) {
+            $this->SetFont("",$current);
+        }
+
+        return $return;
+
+    }
+
+
+    public function moveX($move) {
+
+        $x = $this->getX();
+
+        $x += $move;
+
+        return $this->setX($x);
+
+    }
+
+
+    public function moveY($move) {
+
+        $y = $this->getY();
+
+        $y += $move;
+
+        return $this->setY($y);
+
+    }
+
+
+    public function bold($on=false) {
+
+        $style = $this->getFontStyle();
+
+        $style = str_replace("B","",$style);
+
+        if($on) {
+            $style .= "B";
+        }
+
+        return $this->setFont("",$style);
+
+    }
+
+
+    public function Header() {
+
+        if(!$func = $this->headerFunction) {
+            return false;
+        }
+
+        $func($this);
+
+        return true;
+
+    }
+
+
+    public function Footer() {
+
+        if(!$func = $this->footerFunction) {
+            return false;
+        }
+
+        $func($this);
+
+        return true;
+
+    }
+
+
+}
